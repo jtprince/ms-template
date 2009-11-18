203846865453afbaa471472f9140bd77422c854a
John Prince <jtprince@gmail.com>
2009-11-18 12:54:26 -0700

    progress

--- website/lib/mspire.rb
+++ website/lib/mspire.rb
@@ -18,7 +18,7 @@ module Mspire
     [
       ['Intro', website_base + "/"],
       ['Source', github_base],
-      ['API', github_base + "/rdoc/"],
+      ['API', website_base + "/rdoc/"],
       ['Issues', github_base + "/issues/"],
       ['Wiki', wiki_base + "/"]
     ]
